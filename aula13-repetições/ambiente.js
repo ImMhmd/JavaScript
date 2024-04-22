d1fcf2cb6b632acaa1f1bf1fb15c4cb01304ccab
João Marcos <166159919+ImMhmd@users.noreply.github.com>
2024-04-21 22:13:36 -0300

    estrutura de repetição

---
+++ aula13-repetições/ambiente.js
@@ -0,0 +1,34 @@
+let c = 1
+
+do{
+    console.log(`Passo ${c}`)
+    c++
+
+} while (c <=6) 
+
+/* Estrutura do while executa o código primeiro e depois faz o teste lógico, possibilitando executar o código pelo menos uma vez mesmo que o teste lógico seje falso */
+
+
+/*
+
+let c = 1
+
+while (c <= 6){
+    console.log(`Passo ${c}`)
+    c++ || c = c + 1 
+
+
+    -- Estrutura while faz o teste lógico no inicio e para até a condição ser falsa, tem a possibilidade de não ser executada
+}
+
+*/
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
