5bd63b41da7a8ba35f0cdb0435a57deae9aad051
João Marcos <166159919+ImMhmd@users.noreply.github.com>
2024-04-26 19:19:04 -0300

    ...

---
+++ aula16/função02.js
@@ -0,0 +1,5 @@
+function soma (n1=0, n2=0){ // Eu posso já atribuir um valor para o parâmetro caso eu não atribua um valor na chamada
+    return n1 + n2
+}
+
+console.log(soma(2))
---
+++ aula16/função03.js
@@ -0,0 +1,5 @@
+let v = function(x){
+    return x*2
+} // No JavaSvcript da para colocar uma função dentro de uma váriavel
+
+console.log(v(5)) 
