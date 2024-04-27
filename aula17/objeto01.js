e3d3be6c1b45388d5bf68b98b35d5f1a6daf916b
João Marcos <166159919+ImMhmd@users.noreply.github.com>
2024-04-27 17:30:51 -0300

    objeto

---
+++ aula17/objeto01.js
@@ -0,0 +1,17 @@
+let amigo = {nome: 'José', 
+sexo: 'M' , 
+peso: 85.4, 
+engordar(p=0){
+    console.log('Engordou')
+    this.peso += p
+}}
+
+amigo.engordadr(2)
+console.log(amigo.nome)
+
+/*
+
+Objetos são váriaveis que além de guardar valores (atributos) os objetos podem guardar funcionalidade que chamamos de métodos
+
+
+*/ 
