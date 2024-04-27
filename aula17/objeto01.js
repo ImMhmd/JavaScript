0312e18c5a57d021909f0abc6d594c134b99ad27
João Marcos <166159919+ImMhmd@users.noreply.github.com>
2024-04-27 19:39:19 -0300

    mudança

--- aula17/objeto01.js
+++ aula17/objeto01.js
@@ -11,11 +11,11 @@ let s = 'Javascript'
 amigo.engordadr(2)
 console.log(amigo.nome)
 
-s.toUpperCase()
 
 /*
 
-Objetos são váriaveis que além de guardar valores (atributos) os objetos podem guardar funcionalidade que chamamos de métodos
+E possui um identificador(atributo) literal para cada valor(espaço) e não apenas 0,1,2,3... igual os vetores. Sendo mais facil a visualização.
 
+Objetos são váriaveis que além de guardar valores (atributos) os objetos podem guardar funcionalidade que chamamos de métodos.
 
 */ 
